14e23e08bb13eefa4f7c6155deb20c1ff945e49e
deryl93 <95095509+deryl93@users.noreply.github.com>
2021-11-26 23:40:34 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import os
 proxy = 'http://user:pass@167.99.236.14:80'
-os.system('curl ipinfo.io')
+os.system('curl ident.me')
 os.environ['http_proxy'] = proxy 
 os.environ['HTTP_PROXY'] = proxy
 os.environ['https_proxy'] = proxy
 os.environ['HTTPS_PROXY'] = proxy
-os.system('curl ipinfo.io')
+os.system('curl ident.me')
