da729b7c2d797f08965a1352917031d0ba387667
deryl93 <95095509+deryl93@users.noreply.github.com>
2021-11-26 23:37:59 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import os
-proxy = '36.67.63.239:38071'
+proxy = 'http://user:pass@167.99.236.14:80'
 os.system('curl ipinfo.io')
 os.environ['http_proxy'] = proxy 
 os.environ['HTTP_PROXY'] = proxy
