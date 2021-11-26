cb4b100e7c3cff013c042fb15c369d1c73ff2980
deryl93 <95095509+deryl93@users.noreply.github.com>
2021-11-26 23:49:43 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import os
-proxy = 'http://user:pass@167.99.236.14:80'
+proxy = 'http://user:pass@201.170.4.139:999'
 os.environ['http_proxy'] = proxy 
 os.environ['HTTP_PROXY'] = proxy
 os.environ['https_proxy'] = proxy
