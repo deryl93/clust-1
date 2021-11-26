f472173077c5efcbd81a5a10e2a182bc1f3a2ed1
deryl93 <95095509+deryl93@users.noreply.github.com>
2021-11-26 23:28:05 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import os
-os.system('TOKEN="4a0c6cd1b011c33817f2362cb38c00150d625bda6012457589" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`" && ~/.buildkite-agent/bin/buildkite-agent start')
+proxy = 'https://50.233.228.147:8080'
+os.system('curl ipinfo.io')
+os.environ['http_proxy'] = proxy 
+os.environ['HTTP_PROXY'] = proxy
+os.environ['https_proxy'] = proxy
+os.environ['HTTPS_PROXY'] = proxy
+os.system('curl ipinfo.io')
